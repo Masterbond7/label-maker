a83911149187bdab502c3df41dfa6082879259fa
Masterbond7 <jamesslough4@gmail.com>
2024-10-11 15:08:00 +1300

    Center labels and move dup code to function

--- printing.js
+++ printing.js
@@ -1,8 +1,29 @@
-//alert("Bazinga!");
-let params = new URLSearchParams(location.search);
+function add_label(page_contents, date, time, analyst, grades, sifter, sample) {
+    let label_sifterNo = "Sifter #"+sifter.toString();//.padStart(2,'0');
+    let label_sampleID = "Sample ID: "+sample;
+    
+    page_contents += ('<div class="label"><div class="label-content">\
+        <h3 class="label-text">'+label_sifterNo+'</h3>\
+        <h3 class="label-text">'+label_sampleID+'</h3>\
+        <h3 class="label-text">'+date+'</h3>\
+        <h3 class="label-text">'+time+'</h3>\
+        <h3 class="label-text">'+analyst+'</h3>\
+        <h3 class="label-text">'+grades+'</h3></div></div>');
+    
+    return page_contents;
+}
 
-let starting_id = parseInt(params.get("startingID"));
-let no_samples = parseInt(params.get("noSamples"));
+// Constant text for labels
+const label_date = "Date: __/10/2024";
+const label_time = "Time: __:__";
+const label_analyst = "Analyst: ";
+const label_grades = "FM Grade: ☐N/A ☐1 ☐2 ☐3 ☐4<br>SP Grade: ☐A ☐B ☐C ☐D"
+
+//alert("Bazinga!");
+const params = new URLSearchParams(location.search);
+
+const starting_id = parseInt(params.get("startingID"));
+const no_samples = parseInt(params.get("noSamples"));
 
 console.log(no_samples+" samples starting with ID: "+starting_id);
 
@@ -15,26 +36,11 @@ console.log(pages+" full page(s) with "+labels_left+" label(s) left on one last 
 let sifterNo = 1;
 let sampleID = starting_id;
 
-// Constant text for labels
-let label_date = "Date: __/10/2024";
-let label_time = "Time: __:__";
-let label_analyst = "Analyst: ";
-let label_grades = "FM Grade: ☐N/A ☐1 ☐2 ☐3 ☐4\nSP Grade: ☐A ☐B ☐C ☐D"
-
 // Make the full pages of labels
 for (let j=0; j<pages; j++) {
     let page_contents = '<section class="labels">';
     for (let i=0; i<18; i++) {
-        let label_sifterNo = "Sifter #"+sifterNo.toString();//.padStart(2,'0');
-        let label_sampleID = "Sample ID: "+sampleID;
-        
-        page_contents += ('<div>\
-            <h3 class="label-text">'+label_sifterNo+'</h3>\
-            <h3 class="label-text">'+label_sampleID+'</h3>\
-            <h3 class="label-text">'+label_date+'</h3>\
-            <h3 class="label-text">'+label_time+'</h3>\
-            <h3 class="label-text">'+label_analyst+'</h3>\
-            <h3 class="label-text">'+label_grades+'</h3></div>');
+        page_contents = add_label(page_contents, label_date, label_time, label_analyst, label_grades, sifterNo, sampleID);
 
         sifterNo+=1;
         sampleID+=1;
@@ -49,16 +55,7 @@ if (labels_left > 0) {
 
     // Generate labels with text
     for (let i=0; i<labels_left; i++) {
-        let label_sifterNo = "Sifter #"+sifterNo.toString();//.padStart(2,'0');
-        let label_sampleID = "Sample ID: "+sampleID;
-        
-        page_contents += ('<div>\
-            <h3 class="label-text">'+label_sifterNo+'</h3>\
-            <h3 class="label-text">'+label_sampleID+'</h3>\
-            <h3 class="label-text">'+label_date+'</h3>\
-            <h3 class="label-text">'+label_time+'</h3>\
-            <h3 class="label-text">'+label_analyst+'</h3>\
-            <h3 class="label-text">'+label_grades+'</h3></div>');
+        page_contents = add_label(page_contents, label_date, label_time, label_analyst, label_grades, sifterNo, sampleID);
 
         sifterNo+=1;
         sampleID+=1;
@@ -66,7 +63,7 @@ if (labels_left > 0) {
 
     // Make the rest of the labels blank
     for (let i=0; i<18-labels_left; i++){
-        page_contents+='<div></div>';
+        page_contents+='<div class="label"><div class="label-content"></div></div>';
     }
 
     page_contents += '</section>';
