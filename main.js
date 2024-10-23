3cd1a8558814a066eec2bc3c92aa6d1a97cc514f
Masterbond7 <47312040+Masterbond7@users.noreply.github.com>
2024-10-23 14:36:33 +1300

    Fixed maths and added confirm button if "spare" labels are being generated

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ function submit_form(event) {
     event.preventDefault();
 
     // Get form data
-    let starting_id = document.getElementById("startingID").value;
-    let num_samples = document.getElementById("noSamples").value;
+    let starting_id = parseInt(document.getElementById("startingID").value);
+    let num_samples = parseInt(document.getElementById("noSamples").value);
 
     let used_labels = 0;
     if (document.getElementById("partialSheet").checked) {
-        used_labels = document.getElementById("usedLabels").value;
+        used_labels = parseInt(document.getElementById("usedLabels").value);
     }
 
     let empty_labels = 0;
@@ -18,8 +18,15 @@ function submit_form(event) {
 
     // Calculate number of leftover labels
     let num_leftover = 0;
-    num_leftover = 18 - ((used_labels + num_samples) % 18);
-    alert(num_leftover);
+    num_leftover = 18-((used_labels + num_samples) % 18);
+    if (num_leftover == 18) {
+        num_leftover = 0;
+    }
+    
+    let print = true;
+    if (num_leftover > 0) {
+        print = confirm("If you continue "+num_leftover+" \"spare\" label(s) will be generated, are you sure?");
+    }
 
     let month = document.getElementById("labelMonth").value;
     if (month=="") {month = "NA";}
@@ -27,7 +34,9 @@ function submit_form(event) {
     if (year=="") {year = "NA";}
 
     // Open printing window
-    window.open("printing.html?startingID="+starting_id+"&noSamples="+num_samples+"&usedLabels="+used_labels+"&month="+month+"&year="+year+"&emptyLabels="+empty_labels);
+    if (print) {
+        window.open("printing.html?startingID="+starting_id+"&noSamples="+num_samples+"&usedLabels="+used_labels+"&month="+month+"&year="+year+"&emptyLabels="+empty_labels);
+    }
 }
 
 // Function that is called when partialSheet checkbox is toggled
