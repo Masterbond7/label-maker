76ff7fc02b9d50ae4d58c545389f661dc0633d50
Masterbond7 <47312040+Masterbond7@users.noreply.github.com>
2024-11-07 15:41:26 +1300

    mmm yummy pictograms

--- Scripts/reagent-printing.js
+++ Scripts/reagent-printing.js
@@ -1,6 +1,6 @@
 // Define function to generate empty label
 function gen_reagent_label(pictogram, comm_name, full_name, reagent_id_text, time_text, date_text, creator_text, expiry_text) {
-    let label_contents = '<div class="label"><section><div class="reagent-label-content">\
+    let label_contents = '<div class="label"><section style="width:80%;"><div class="reagent-label-content">\
     <h2 class="label-text">'+comm_name+'</h2>\
     <h5 class="label-text">'+full_name+'</h5>\
     <h4 class="label-text">'+reagent_id_text+'</h4>\
@@ -8,7 +8,7 @@ function gen_reagent_label(pictogram, comm_name, full_name, reagent_id_text, tim
     <h4 class="label-text">'+date_text+'</h4>\
     <h4 class="label-text">'+creator_text+'</h4>\
     <h4 class="label-text">'+expiry_text+'</h4></div></section>\
-    <section><img src="../Images/Hazards/ascorbic_acid_solution.png"  width="20%"></section></div>';
+    <section style="width:20%;"><img src="../Images/Hazards/'+pictogram+'.png" width="100%"></section></div>';
     return label_contents;
 }
 
@@ -28,7 +28,7 @@ let page_contents = '<section class="labels">';
 
 // Make 18 labels
 for (let label_no=0; label_no<18; label_no++) {
-    page_contents += gen_reagent_label("", "Ascorbic Acid", "L-Ascorbic Acid + Metaphosphoric Acid + Acetic acid + EDTA", label_reagent_id, label_time, label_date, label_creator, label_expiry);
+    page_contents += gen_reagent_label(reagent, "Ascorbic Acid", "L-Ascorbic Acid + Metaphosphoric Acid + Acetic acid + EDTA", label_reagent_id, label_time, label_date, label_creator, label_expiry);
 }
 
 // End page and add append to site
