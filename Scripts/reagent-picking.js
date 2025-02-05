db2502a81fe1172230f7161faf38a2fe7e9cae9e
Masterbond7 <47312040+Masterbond7@users.noreply.github.com>
2025-02-06 01:43:33 +1300

    arg arg arg stupid js

--- Scripts/reagent-picking.js
+++ Scripts/reagent-picking.js
@@ -1,9 +1,9 @@
 function label_time(type, reagent) { // :D
-    if (document.getElementById("noLabels").value == '') { let num_labels = "1"; }
-    else { let num_labels = document.getElementById("noLabels").value; }
+    let num_labels = "0";
+    if (document.getElementById("noLabels").value != '') { num_labels = document.getElementById("noLabels").value; }
 
-    if (document.getElementById("usedLabels").value == '') { let used_labels = "0"; }
-    else { let used_labels = document.getElementById("usedLabels").value; }
+    let used_labels = "0";
+    if (document.getElementById("usedLabels").value != '') { used_labels = document.getElementById("usedLabels").value; }
 
     window.open("./reagent-printing.html?reagent="+reagent+"&label_type="+type+"&num_labels="+num_labels+"&used_labels="+used_labels);
 }
