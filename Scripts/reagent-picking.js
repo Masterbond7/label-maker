cd822c375feee049ccd5aed0c822fde430ef13b7
Masterbond7 <47312040+Masterbond7@users.noreply.github.com>
2025-02-06 01:33:16 +1300

    All the data!

--- Scripts/reagent-picking.js
+++ Scripts/reagent-picking.js
@@ -1,3 +1,6 @@
 function label_time(type, reagent) { // :D
-    alert(type+" ("+reagent+")");
+    let num_labels = parseInt(document.getElementById("noLabels").value);
+    let used_labels = parseInt(document.getElementById("usedLabels").value);
+
+    alert(type+" ("+reagent+")\n"+String(num_labels)+" "+String(used_labels));
 }
