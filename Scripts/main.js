1ae27295a294712301cac4feae957dd92e0eb217
Masterbond7 <jamesslough4@gmail.com>
2025-02-08 14:34:44 +1300

    Lil preview window with NON responsive scaling

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,24 @@
+// On form submission open new page
 function submit_form(event) {
+    // Stop page from refreshing
+    event.preventDefault();
+
+    let result = get_form_data(event);
+
+    if (result!="N/A") {window.open(result);}
+}
+
+// On form update, update preview
+function update_preview(event) {
+    // Stop page from refreshing
+    event.preventDefault();
+
+    let result = get_form_data(event);
+    if (result!="N/A") {document.getElementById("preview_iframe").setAttribute('src', result)}
+}
+
+// Function gets and returns form data
+function get_form_data(event) {
     // Stop page from refreshing
     event.preventDefault();
 
@@ -62,36 +82,13 @@ function submit_form(event) {
 
     // Open printing window
     if (print) {
-        window.open("./Pages/printing.html?startingID="+starting_id+"&noSamples="+num_samples+"&usedLabels="+used_labels+"&month="+month+"&year="+year+"&emptyLabels="+empty_labels+"&sampleBlanks="+sample_blanks);
+        return ("./Pages/printing.html?startingID="+starting_id+"&noSamples="+num_samples+"&usedLabels="+used_labels+"&month="+month+"&year="+year+"&emptyLabels="+empty_labels+"&sampleBlanks="+sample_blanks);
     } else { // Cleanup anyway if not printing
         localStorage.removeItem("missing_ids");
     }
-}
 
-// Function that is called when partialSheet checkbox is toggled
-function partial_sheet_checkbox() {
-    var effected_elements = document.getElementsByClassName("partialSheetData");
-    console.log(effected_elements);
-
-    // Partial sheet being used
-    if (document.getElementById('partialSheet').checked) {
-        document.getElementById('usedLabels').disabled=false;
-        
-        for (let i=0; i<effected_elements.length; i++) {
-            effected_elements[i].style.visibility="visible";
-            console.log(effected_elements[i].style.visibility);
-        }
-    } 
-
-    // Full sheet being used
-    else {
-        document.getElementById('usedLabels').disabled=true;
-        
-        for (let i=0; i<effected_elements.length; i++) {
-            effected_elements[i].style.visibility="hidden";
-            console.log(effected_elements[i].style.visibility);
-        }
-    }
+    // If here something has gone wrong, return N/A
+    return "N/A"
 }
 
 // Run init
